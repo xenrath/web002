2b61523d84d68edbdfdfd2a70f0e9365dfde3531
xenrath <xenrath89@gmail.com>
2020-10-10 11:21:36 +0700

    Menampilkan Data dari Beberapa Relasi Tabel dengan JOIN SQL (23)

--- config/config.php
+++ config/config.php
@@ -21,4 +21,11 @@
 			return $base_url;
 		}
 	}
+
+	function tgl_indo($tgl){
+		$tanggal = substr($tgl, 8, 2);
+		$bulan = substr($tgl, 5, 2);
+		$tahun = substr($tgl, 0, 4);
+		return $tanggal."/".$bulan."/".$tahun;
+	}
  ?>
--- rekam_medis/data.php
+++ rekam_medis/data.php
@@ -13,24 +13,58 @@
 	</h4>
 	<form method="post" name="proses">
 		<div class="table-responsive">
-			<table class="table table-striped table-bordered table-hover" id="dokter">
+			<table class="table table-striped table-bordered table-hover" id="rekam_medis">
 				<thead>
 					<tr>
-						<th>
-							<center>
-								<input type="checkbox" id="select_all" value="">
-							</center>
-						</th>
 						<th>No.</th>
 						<th>Tanggal Periksa</th>
 						<th>Nama Pasien</th>
 						<th>Keluhan</th>
 						<th>Nama Dokter</th>
+						<th>Diagnosa</th>
+						<th>Poliklinik</th>
+						<th>Data Obat</th>
 						<th><i class="glyphicon glyphicon-cog"></i></th>
 					</tr>
 				</thead>
 				<tbody>
-					
+					<?php 
+						$no = 1;
+						$query = "SELECT * FROM tb_rekammedis 
+							INNER JOIN tb_pasien ON tb_rekammedis.id_pasien = tb_pasien.id_pasien 
+							INNER JOIN tb_dokter ON tb_rekammedis.id_dokter = tb_dokter.id_dokter
+							INNER JOIN tb_poliklinik ON tb_rekammedis.id_poli = tb_poliklinik.id_poli";
+						$sql_rm = mysqli_query($con, $query) or die (mysqli_error($con));
+						while ($data = mysqli_fetch_array($sql_rm)) {
+					?>
+					<tr>
+						<td><?= $no++ ?></td>
+						<td><?= tgl_indo($data['tgl_periksa']) ?></td>
+						<td><?= $data['nama_pasien'] ?></td>
+						<td><?= $data['keluhan'] ?></td>
+						<td><?= $data['nama_dokter'] ?></td>
+						<td><?= $data['diagnosa'] ?></td>
+						<td><?= $data['nama_poli'] ?></td>
+						<td>
+							<?php 
+								$sql_obat = mysqli_query($con, "SELECT * FROM tb_rm_obat INNER JOIN tb_obat ON tb_rm_obat.id_obat = tb_obat.id_obat WHERE id_rm = '$data[id_rm]'") or die (mysqli_error($con));
+								while ($data_obat = mysqli_fetch_array($sql_obat)) {
+									echo $data_obat['nama_obat']."<br>";
+								}
+							 ?>
+						</td>
+						<td align="center">
+							<a href="edit.php?id=<?= $data['id_rm'] ?>" class="btn btn-warning btn-xs">
+								<i class="glyphicon glyphicon-edit"></i>
+							</a>
+							<a href="del.php?id=<?= $data['id_rm'] ?>" onclick="return confirm('Yakin manghapus data?')" class="btn btn-danger btn-xs">
+								<i class="glyphicon glyphicon-trash"></i>
+							</a>
+						</td>
+					</tr>
+					<?php 
+						}
+					 ?>
 				</tbody>
 			</table>
 		</div>
@@ -38,6 +72,17 @@
 	<div class="box pull-right">
 		<button class="btn btn-danger btn-sm" onclick="hapus()"><i class="glyphicon glyphicon-trash"></i> Hapus</button>
 	</div>
+	<script>
+	$(document).ready(function(){
+		$('#rekam_medis').DataTable({
+			columnDefs: [{
+				"searchable" : false,
+				"orderable" : false,
+				targets: 8
+			}]
+		});
+	});
+	</script>
 </div>
 
 <?php 
